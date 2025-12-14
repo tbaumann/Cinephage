d55f2e59701ac3150febb1488848958b928f56b8
root <root@Cinephage.internal>
2025-12-14 23:25:59 +0000

    style: format NZBGet client and docker files

--- src/lib/server/downloadClients/nzbget/types.ts
+++ src/lib/server/downloadClients/nzbget/types.ts
@@ -1,80 +1,80 @@
 export interface NzbgetGroup {
-    Name: string;
-    DestDir: string;
+	Name: string;
+	DestDir: string;
 }
 
 export interface NzbgetStatus {
-    Version: string;
-    DownloadRate: number;
+	Version: string;
+	DownloadRate: number;
 }
 
 export interface NzbgetList {
-    /** NZB ID */
-    FirstID: number;
-    /** NZB ID */
-    LastID: number;
-    /** NZB Name */
-    NZBName: string;
-    /** NZB Filename */
-    NZBFilename: string;
-    /** Category */
-    Category: string;
-    /** Size in Bytes (Lo part) */
-    FileSizeLo: number;
-    /** Size in Bytes (Hi part) */
-    FileSizeHi: number;
-    /** Remaning Size in Bytes (Lo part) */
-    RemainingSizeLo: number;
-    /** Remaning Size in Bytes (Hi part) */
-    RemainingSizeHi: number;
-    /** Download Status (QUEUED, PAUSED, DOWNLOADING, SUCCESS, FAILURE, DELETED) */
-    Status: string;
-    /** Destination Directory */
-    DestDir: string;
-    /** Download Priority (Start, Pforce, Force, High, Normal, Low, VeryLow) */
-    Priority: string;
+	/** NZB ID */
+	FirstID: number;
+	/** NZB ID */
+	LastID: number;
+	/** NZB Name */
+	NZBName: string;
+	/** NZB Filename */
+	NZBFilename: string;
+	/** Category */
+	Category: string;
+	/** Size in Bytes (Lo part) */
+	FileSizeLo: number;
+	/** Size in Bytes (Hi part) */
+	FileSizeHi: number;
+	/** Remaning Size in Bytes (Lo part) */
+	RemainingSizeLo: number;
+	/** Remaning Size in Bytes (Hi part) */
+	RemainingSizeHi: number;
+	/** Download Status (QUEUED, PAUSED, DOWNLOADING, SUCCESS, FAILURE, DELETED) */
+	Status: string;
+	/** Destination Directory */
+	DestDir: string;
+	/** Download Priority (Start, Pforce, Force, High, Normal, Low, VeryLow) */
+	Priority: string;
 }
 
 export interface NzbgetHistory {
-    /** NZB ID */
-    ID: number;
-    /** NZB Name */
-    Name: string;
-    /** NZB Filename */
-    NZBFilename: string;
-    /** Category */
-    Category: string;
-    /** Size in Bytes (Lo part) */
-    FileSizeLo: number;
-    /** Size in Bytes (Hi part) */
-    FileSizeHi: number;
-    /** Download Status (SUCCESS, FAILURE, DELETED) */
-    Status: string;
-    /** Destination Directory */
-    DestDir: string;
-    /** Download Priority (Start, Pforce, Force, High, Normal, Low, VeryLow) */
-    Priority: string;
-    /** Completed Time (Low part) */
-    UnixTimeLo: number;
-    /** Completed Time (Hi part) */
-    UnixTimeHi: number;
+	/** NZB ID */
+	ID: number;
+	/** NZB Name */
+	Name: string;
+	/** NZB Filename */
+	NZBFilename: string;
+	/** Category */
+	Category: string;
+	/** Size in Bytes (Lo part) */
+	FileSizeLo: number;
+	/** Size in Bytes (Hi part) */
+	FileSizeHi: number;
+	/** Download Status (SUCCESS, FAILURE, DELETED) */
+	Status: string;
+	/** Destination Directory */
+	DestDir: string;
+	/** Download Priority (Start, Pforce, Force, High, Normal, Low, VeryLow) */
+	Priority: string;
+	/** Completed Time (Low part) */
+	UnixTimeLo: number;
+	/** Completed Time (Hi part) */
+	UnixTimeHi: number;
 }
 
 export interface NzbgetConfigResponse {
-    result: NzbgetConfigItem[];
+	result: NzbgetConfigItem[];
 }
 
 export interface NzbgetConfigItem {
-    Name: string;
-    Value: string;
+	Name: string;
+	Value: string;
 }
 
 export interface JsonRpcResponse<T> {
-    version: string;
-    result: T;
-    error: {
-        name: string;
-        code: number;
-        message: string;
-    } | null;
+	version: string;
+	result: T;
+	error: {
+		name: string;
+		code: number;
+		message: string;
+	} | null;
 }
